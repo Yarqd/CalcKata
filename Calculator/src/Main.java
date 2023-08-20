86ad2604ccd2c11bc36d0b8a9feae26cc3e5d696
Iaroslav Rodionov <steeltraser@gmail.com>
2023-08-20 16:12:49 +0300

    2

--- Calculator/src/Main.java
+++ Calculator/src/Main.java
@@ -1,52 +1,52 @@
+import java.io.IOException;
 import java.util.Objects;
 import java.util.Scanner;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Давай посчитаем?");
-        String primer; //создать строку
-        Scanner in = new Scanner(System.in); //Вызвать сканер
-        primer = in.nextLine(); //записать пример в primer
-        //System.out.println(primer); //показать то что введено
-        String [] a = primer.split(" "); //разделить на массивы через пробел
+        Scanner in = new Scanner(System.in);
+        String primer = in.nextLine();
+        String [] a = primer.split(" ");
         for (String element: a) {
-        //    System.out.print(element); //вывести элеиенты с новой строки каждый
         }
         System.out.println();
-        int b = Integer.parseInt (a[0]); //Привести к инт
-        int c = Integer.parseInt(a[2]);  //Привести к инт
-        char[] e = a[1].toCharArray();   //привести к чар
-        //System.out.println(e);
 
-        switch (e[0]){
-            case ('+'):
-                System.out.println(b+c);
-                break;
-
-            case ('-'):
-                System.out.println(b-c);
-                break;
-
-            case ('*'):
-                System.out.println(b*c);
-                break;
-
-            case ('/'):
-                System.out.println(b/c);
-                break;
-
-            default:
-                System.out.println("NEPRAVILNO");
-                break;
-
-//        while (Objects.equals(a[1], "+")) {
-//            System.out.println(b+c);
-//            break;
-
+        if (a[0] == "I|II|III|IV|V|VI|VII|VIII|IX|X" ^ a[2] == "I|II|III|IV|V|VI|VII|VIII|IX|X"){
 
         }
 
-     }
-    }
 
+        int b = Integer.parseInt (a[0]);
+        int c = Integer.parseInt(a[2]);
+        char[] e = a[1].toCharArray();
+
+        if (b < 0 | b>10 | c<0 | c>10) {
+        System.out.println("Вы ввели недопустимые числа");
+        System. exit(0);
+        }
+
+        else
+            switch (e[0]) {
+                    case ('+'):
+                        System.out.println(b + c);
+                        break;
+
+                    case ('-'):
+                        System.out.println(b - c);
+                        break;
+
+                    case ('*'):
+                        System.out.println(b * c);
+                        break;
+
+                    case ('/'):
+                        System.out.println(b / c);
+                        break;
+
+                    default:
+                        throw new IOException();
+                }
+            }
+        }
