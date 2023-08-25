c22dbf72efb3063f49847e07234138d4fbc56d5c
Iaroslav Rodionov <steeltraser@gmail.com>
2023-08-25 18:29:04 +0300

    2

--- Calculator/src/Main.java
+++ Calculator/src/Main.java
@@ -1,68 +1,77 @@
 import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static int main(String[] args) throws IOException, NullPointerException {
         System.out.println("Давай посчитаем?");
         Scanner in = new Scanner(System.in);
         String primer = in.nextLine();
-        String [] a = primer.split(" ");
+        String[] a = primer.split(" ");
 
-//        Object obj = a[0];
-//        Object obj2 = a[2];
-//        if (obj instanceof String || obj2 instanceof String) {
-//            System.out.println("Переменная obj является строкой");
-//        } else {
-//            System.out.println("Переменная obj не является строкой");
-//        }
-//        System.out.println(a[0] + a[2]);
-
-//        if (a[0] == int || a[2] == int) {
-//            break;
-//        }
-
-
-        char z = ""
-
-
-        if (obj == "I|II|III|IV|V|VI|VII|VIII|IX|X" ^ obj2 == "|II|III|IV|V|VI|VII|VIII|IX|X");
-        else {
-            System.out.println("No Rome");
-        }
-//
-//        }
-
-
-        int b = Integer.parseInt (a[0]);
-        int c = Integer.parseInt(a[2]);
+        int b;
+        int c;
+        boolean isRomain;
         char[] e = a[1].toCharArray();
+        try {
+            b = Integer.parseInt(a[0]);
+            c = Integer.parseInt(a[2]);
 
-        if (b < 0 | b>10 | c<0 | c>10) {
-        System.out.println("Вы ввели недопустимые числа");
-        System. exit(0);
+        } catch (NullPointerException e1) {
+            System.out.println("Вы ввели неправильный знак");
         }
 
-        else
+        if (b < 0 | b > 10 | c < 0 | c > 10) {
+            System.out.println("Вы ввели недопустимые числа");
+            System.exit(0);
+        } else
             switch (e[0]) {
-                    case ('+'):
-                        System.out.println(b + c);
-                        break;
-
-                    case ('-'):
-                        System.out.println(b - c);
-                        break;
-
-                    case ('*'):
-                        System.out.println(b * c);
-                        break;
-
-                    case ('/'):
-                        System.out.println(b / c);
-                        break;
-
-                    default:
-                        throw new IOException();
-                }
+                case ('+') -> System.out.println(b + c);
+                case ('-') -> System.out.println(b - c);
+                case ('*') -> System.out.println(b * c);
+                case ('/') -> System.out.println(b / c);
+                default -> throw new IOException();
             }
-        }
+    }
 
+    private static int romanToNumber (String roman) throws InputMismatchException {
+        try {
+            if (roman.equals("I")) {
+                return 1;
+            } else if (roman.equals("II")) {
+                return 2;
+            } else if (roman.equals("III")) {
+                return 3;
+            } else if (roman.equals("IV")) {
+                return 4;
+            } else if (roman.equals("V")) {
+                return 5;
+            } else if (roman.equals("VI")) {
+                return 6;
+            } else if (roman.equals("VII")) {
+                return 7;
+            } else if (roman.equals("VIII")) {
+                return 8;
+            } else if (roman.equals("IX")) {
+                return 9;
+            } else if (roman.equals("X")) {
+                return 10;
+            }
+        } catch (InputMismatchException e) {
+            throw new InputMismatchException("Неверный формат данных");
+        }
+        return -1;
+    }
+
+    private static String convertNumToRoman (int arab) {
+        String[] roman = {"O", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
+                "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL",
+                "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX",
+                "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX",
+                "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX",
+                "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC",
+                "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"
+        };
+        final String s = roman[arab];
+        return s;
+}
